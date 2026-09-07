64c16f2b705e845f18e1579d929acbfceec566cb
Yashraj Singh <Yashraj2013216@akgec.ac.in>
2026-09-07 16:33:29 +0530

    Small Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import AppRoutes from "./routes/AppRoutes";
+
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import TasksPage from "./pages/TasksPage";
 
 function App() {
-  return (
-      <>
-        <Navbar />
-        <hr />
-        <AppRoutes />
-      </>
-  );
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    return (
+        <>
+            <Navbar />
+
+            <Routes>
+                {/* Default Route */}
+                <Route
+                    path="/"
+                    element={
+                        user ? (
+                            <Navigate to="/dashboard" replace />
+                        ) : (
+                            <Navigate to="/login" replace />
+                        )
+                    }
+                />
+
+                {/* Public Routes */}
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+
+                {/* Protected Pages */}
+                <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/projects" element={<ProjectsPage />} />
+                <Route path="/tasks" element={<TasksPage />} />
+
+                {/* Invalid URL */}
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
