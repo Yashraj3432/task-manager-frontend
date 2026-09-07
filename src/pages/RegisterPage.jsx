2709312069a6b8e1fba4fc8a238600da3fa13540
Yashraj Singh <Yashraj2013216@akgec.ac.in>
2026-09-07 11:49:42 +0530

    Implement controlled register form using useState

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,19 +1,58 @@
+import { useState } from "react";
+
 function RegisterPage() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log({
+            name,
+            email,
+            password,
+        });
+    };
+
     return (
         <div>
             <h1>Register</h1>
 
-            <form>
-                <input type="text" placeholder="Name" />
+            <form onSubmit={handleSubmit}>
+
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+
                 <br /><br />
 
-                <input type="email" placeholder="Email" />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
                 <br /><br />
 
-                <input type="password" placeholder="Password" />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
                 <br /><br />
 
-                <button>Register</button>
+                <button type="submit">
+                    Register
+                </button>
+
             </form>
         </div>
     );
