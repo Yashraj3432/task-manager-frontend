788e570defaa27bca6c5ada6f5e14589d56a9d23
Yashraj Singh <Yashraj2013216@akgec.ac.in>
2026-09-07 12:10:53 +0530

    Implement login API and React login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,16 +1,53 @@
+import { useState } from "react";
+import api from "../services/api";
+
 function LoginPage() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await api.post("/users/login", {
+                email,
+                password,
+            });
+
+            console.log(response.data);
+
+            alert(`Welcome ${response.data.name}!`);
+
+        } catch (error) {
+            alert(error.response?.data?.message || "Login failed");
+        }
+    };
+
     return (
         <div>
             <h1>Login</h1>
 
-            <form>
-                <input type="email" placeholder="Email" />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                />
+
                 <br /><br />
 
-                <input type="password" placeholder="Password" />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                />
+
                 <br /><br />
 
-                <button>Login</button>
+                <button type="submit">Login</button>
             </form>
         </div>
     );
